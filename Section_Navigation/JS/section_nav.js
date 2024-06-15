466bc3cac38bcad1b47b8449fb7350b54986c725
Abhijith Acharya <abhijith.acharya@epsilon.com>
2024-06-15 19:45:40 +0530

    section nav sticky functionality added

--- Section_Navigation/JS/section_nav.js
+++ Section_Navigation/JS/section_nav.js
@@ -2,6 +2,28 @@
 $(document).ready(function(){
     
     //DOM selections
+    const headerEle = $(".header");
+    const sectionNav = $(".section-nav");
+    const heroBg = $(".hero-bg");
 
     //Events
+    $(window).scroll(function(){
+        checkScroll();
+    })
+
+    function checkScroll(){
+        let headerHeight = headerEle.outerHeight();
+        let heroBgHeight = heroBg.outerHeight();
+        
+        let scrollPosition = $(window).scrollTop();
+        if(scrollPosition >= heroBgHeight){
+            sectionNav.addClass('fixed-section-nav')
+            sectionNav.css("top", headerHeight);
+        }
+        else{
+            sectionNav.removeClass('fixed-section-nav')
+            sectionNav.css("top", "")
+        }
+    }
 })
+
